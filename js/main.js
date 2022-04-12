76b1a2c59942ec8ce3e06e33b379bd694d5bac0d
Gonzalo <gonzomartinarango@gmail.com>
2022-04-12 12:29:32 -0400

    Initial setup done with desing and markup

--- js/main.js
+++ js/main.js
@@ -1,18 +1,9 @@
-// this is a self-invoking anonymus function
-// it's also called a module [my variation of the module pattern]
-// it encapsule all of your cystom code and make sit private.
-
 (() => {
-  // make asociations (select) with elements in the HTML markup using CSS selectors.
-  let theHeading = document.querySelector(".main-heading");
 
-  //scrip your behavior with functions
-  function logElement() {
-    console.log('clicked on the heading');
-  }
+  const houseInfo = document.querySelector('.house-info');
 
-  // add event handling here (user "triggers")
-  // assign the event you want to lister for, and the function that sould be run
-  theHeading.addEventListener("click", logElement);
+  const houseData = [
+    `I was born in Cali, Colombia, and raised in Palmira. Since I was a little kid I’ve always been really into artistic and manual stuff, my passion when I was a youngman was playing with clay, i used to do it everyday with my dad, he was the real artist at that time. Since then as any other human I have been going through different circumstances and now I'm a professional Visual Communication Designer from the Javeriana University in Cali, Colombia and nowadays I'm studying Interactive Media Design at Fanshawe College in Canada.`, // agregar coma siempre para poder que el siguiente texto se cuente.
 
-})();
+    `Maria is my wife and the complement that gives a head to everything in my life, she is my art director and have been the one behind any radical change that have occur during the develope of all the projects i have done. She is by far the more loving and caring women, she has been my dream team for sure.`
+  ]
